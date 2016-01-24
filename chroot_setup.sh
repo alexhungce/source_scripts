2560e1acecf8104ff320e4a4f973108bea04a8c9
Alex Hung <alex.hung@canonical.com>
2016-01-24 16:20:22 +0800

    chroot: setup chroot for compiling Ubuntu kernels
    Signed-off-by: Alex Hung <alex.hung@canonical.com>

---
+++ chroot_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+shopt -s -o nounset
+
+readonly UBUNTU=( trusty vivid wily xenial )
+
+SOURCE_DIRECTORY=src
+
+[ -e $SOURCE_DIRECTORY ] || mkdir $SOURCE_DIRECTORY
+cd $SOURCE_DIRECTORY
+
+# download kteam-tools
+git clone git://kernel.ubuntu.com/ubuntu/kteam-tools.git
+cd kteam-tools/chroot-setup
+sudo mkdir -p /usr3/chroots 
+
+# ubuntu flavours
+for i in "${UBUNTU[@]}"
+do
+	sudo ./make_chroot $i amd64 
+done
+
+echo ""
+echo "== add username to the sbuild in \"/etc/group\" and reboot =="
