e4a7bef93ac628faa600c1ced391891c1b485015
Alex Hung <alexhung@gmail.com>
2017-12-14 11:50:04 +0800

    analyse: add a script that lists updates of interests
    Signed-off-by: Alex Hung <alexhung@gmail.com>

---
+++ analyse_kernel_update.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+DATE=`date +%F`
+git pull >> pull_history.${DATE}
+
+# acpi
+cat pull_history.${DATE} | grep 'drivers\/acpi' >> filter_history.${DATE}
+cat pull_history.${DATE} | grep 'drivers\/platform\/x86\/' | grep -E 'asus|hp|dell|intel|thinkpad' >> filter_history.${DATE}
+cat pull_history.${DATE} | grep 'drivers\/input\/misc\/soc_button_array.c' >> filter_history.${DATE}
+
+# efi
+cat pull_history.${DATE} | grep 'efi\.[ch]' >> filter_history.${DATE}
+
+# input events
+cat pull_history.${DATE} | grep 'input-event-codes.h' >> filter_history.${DATE}
+
+vi filter_history.${DATE}
