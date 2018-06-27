7a2a3545d9cf8a8c3f43e7c17f11a2debf1a17e2
Alex Hung <alex.hung@canonical.com>
2018-06-26 22:40:06 -0700

    source: fix a typo $1 to $i
    Signed-off-by: Alex Hung <alex.hung@canonical.com>

--- source_download.sh
+++ source_download.sh
@@ -46,7 +46,7 @@ fi
 
 for i in "${UBUNTU[@]}"
 do
-	[ -e ubuntu-$i ] || git clone git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/$i ubuntu-$1
+	[ -e ubuntu-$i ] || git clone git://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/$i ubuntu-$i
 done
 
 
