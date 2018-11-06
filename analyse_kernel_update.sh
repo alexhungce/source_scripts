d5d8b1a6fee5a3faf79c0ad01539541eec36c01c
Alex Hung <alex.hung@canonical.com>
2018-11-06 22:41:17 +0800

    analyse: add suspend.c|h
    Signed-off-by: Alex Hung <alex.hung@canonical.com>

--- analyse_kernel_update.sh
+++ analyse_kernel_update.sh
@@ -8,6 +8,9 @@ cat pull_history.${DATE} | grep 'drivers\/acpi' >> filter_history.${DATE}
 cat pull_history.${DATE} | grep 'drivers\/platform\/x86\/' | grep -E 'asus|hp|dell|intel|thinkpad' >> filter_history.${DATE}
 cat pull_history.${DATE} | grep 'drivers\/input\/misc\/soc_button_array.c' >> filter_history.${DATE}
 
+# suspend
+cat pull_history.${DATE} | grep 'suspend\.[ch]' >> filter_history.${DATE}
+
 # efi
 cat pull_history.${DATE} | grep 'efi\.[ch]' >> filter_history.${DATE}
 
