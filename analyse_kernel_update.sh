53c90eb8b86378adf19380306cc848a6ae399b08
Alex Hung <alex.hung@canonical.com>
2019-02-27 11:27:03 -0800

    analyse: put all files into an array
    Signed-off-by: Alex Hung <alex.hung@canonical.com>

--- analyse_kernel_update.sh
+++ analyse_kernel_update.sh
@@ -1,31 +1,27 @@
-#!/bin/sh
+#!/bin/bash
+shopt -s -o nounset
 
 DATE=`date +%F`
+
+readonly DRIVERS=( drivers/acpi
+		   drivers/platform/x86/dell*
+		   drivers/platform/x86/hp*
+		   drivers/platform/x86/intel*
+		   drivers/platform/x86/thinkpad_acpi.c
+		   drivers/input/misc/soc_button_array.c
+		   *suspend.*
+		   *efi.*
+		   include/uapi/linux/input-event-codes.h
+		   Documentation/admin-guide/kernel-parameters.txt
+		 )
 
 CUR_TAG=$(git describe --tag)
 git pull >> pull_history.${DATE}
 NEW_TAG=$(git describe --tag)
 
-# acpi
-git show $CUR_TAG..$NEW_TAG -- drivers/acpi >> filter_history.${DATE}
-
-# platform drivers
-git show $CUR_TAG..$NEW_TAG -- drivers/platform/x86/dell* >> filter_history.${DATE}
-git show $CUR_TAG..$NEW_TAG -- drivers/platform/x86/hp* >> filter_history.${DATE}
-git show $CUR_TAG..$NEW_TAG -- drivers/platform/x86/intel* >> filter_history.${DATE}
-git show $CUR_TAG..$NEW_TAG -- drivers/platform/x86/thinkpad_acpi.c >> filter_history.${DATE}
-git show $CUR_TAG..$NEW_TAG -- drivers/input/misc/soc_button_array.c >> filter_history.${DATE}
-
-# suspend
-git show $CUR_TAG..$NEW_TAG -- *suspend.* >> filter_history.${DATE}
-
-# efi
-git show $CUR_TAG..$NEW_TAG -- *efi.* >> filter_history.${DATE}
-
-# keymap
-git show $CUR_TAG..$NEW_TAG -- include/uapi/linux/input-event-codes.h >> filter_history.${DATE}
-
-# Documentation
-git show $CUR_TAG..$NEW_TAG -- Documentation/admin-guide/kernel-parameters.txt >> filter_history.${DATE}
+for i in "${DRIVERS[@]}"
+do
+	git show $CUR_TAG..$NEW_TAG -- $i >> filter_history.${DATE}
+done
 
 vi filter_history.${DATE}
