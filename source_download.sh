ec2e1293cd7d45f5504d69b82a32687ce07deed4
Alex Hung <alexhung@gmail.com>
2021-11-08 12:56:08 -0700

    source/chroot: add jammy
    Signed-off-by: Alex Hung <alexhung@gmail.com>

--- source_download.sh
+++ source_download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 shopt -s -o nounset
 
-readonly UBUNTU=( focal impish )
+readonly UBUNTU=( focal impish jammy )
 readonly GITHUB_REPO=( fwdt system_scripts script-fwts lfdk1 hwe_daily debug_scripts )
 
 # assign default directories if there aren't any
